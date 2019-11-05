b52a2f524d630c4e604099505243c582e95c76ef
elijahwildflame <lex.c.rogers@gmail.com>
2019-11-05 10:47:01 -0500

    added body-parser

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # weather-now
+
+get user input of city
+get weather for city
+    if err let the user know there was an err
+    if there is no weather avaiable err
+    else store the weather for the user city
+show weather err
+
+
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
  
 // setting view engine
 app.set('view engine', 'ejs');
 
 // middelware/
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*
     routes
@@ -14,8 +16,12 @@ app.use(express.static('./public'));
 
 // GET/
 app.get('/', function (req, res) {
-  res.render('home.ejs');
-})
+    res.render('home.ejs');
+});
+
+app.post('/whatever-we-want', function(req, res){
+    console.log(req);
+});
  
 app.listen(3000, function(){
     console.log('server is live on port: 3000');
